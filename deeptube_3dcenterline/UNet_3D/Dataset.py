d4cdcdc5c8050b3bf402aad5ddb27afdbd98693c
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-04-18 11:27:56 -0600

    Renaming Training_Dataset_Loader to Dataset. Refactoring function CustomImageDataset3D

--- deeptube_3dcenterline/UNet_3D/Dataset.py
+++ deeptube_3dcenterline/UNet_3D/Dataset.py
@@ -1,8 +1,10 @@
+import numpy as np
+from torch import tensor
 from torch.utils.data import Dataset
 from pathlib import Path
 from .. import input_output as io
 
-class trainingset_dataset_3D(Dataset):
+class CustomImageDataset3D(Dataset):
     def __init__(self, folder_input, folder_target):
         valid_suffix = [".tif", ".tiff"]
         
@@ -14,8 +16,7 @@ class trainingset_dataset_3D(Dataset):
         self.file_names = [p.name for p in Path(self.folder_input).iterdir() if p.suffix in valid_suffix]
         
         #verify each image in input_folder has an associated image in the target folder
-        verify_files_in_target_folder(self.file_names, self.folder_target)
-        
+        verify_files_in_target_folder(self.file_names, self.folder_target)        
     
     def __len__(self):
         return len(self.file_names)
@@ -24,13 +25,17 @@ class trainingset_dataset_3D(Dataset):
     def __getitem__(self, index):
         
         input_img    = io.imread(Path(self.folder_input, self.file_names[index]))
-        input_target = io.imread(Path(self.folder_target, self.file_names[index]))
+        target_img = io.imread(Path(self.folder_target, self.file_names[index]))
+        
+        #converting numpy to tensor
+        input_img = tensor(input_img.astype(np.float32)).float()
+        target_img = tensor(target_img.astype(np.uint8))        
         
         #img is a 3D image with [D,W,H], we only have one channel image, change the dimensions to [1,D,W,H]
-        input_img = input_img[None]
-        input_target = input_target[None]
+        input_img = input_img.unsqueeze(0) if input_img.dim() ==  3 else input_img
+        target_img = target_img.unsqueeze(0) if target_img.dim() == 2 else target_img
         
-        return input_img, input_target
+        return input_img, target_img
         
         
         
