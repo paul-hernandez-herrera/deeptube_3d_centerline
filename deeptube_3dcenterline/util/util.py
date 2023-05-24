1baafe8f30868355cd1245694dfa79919f00ca64
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-05-24 13:26:02 -0600

    adding functionality to allow mhd files in the input files

--- deeptube_3dcenterline/util/util.py
+++ deeptube_3dcenterline/util/util.py
@@ -2,10 +2,13 @@ import tifffile
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import skimage.io as io
 
 def imread(filename):
-    if Path(filename).suffix in {'.tif', '.tiff'}:
+    if Path(filename).suffix.lower() in {'.tif', '.tiff'}:
         return tifffile.imread(filename)
+    if Path(filename).suffix.lower() in {'.mhd'}:
+        return io.imread(filename, plugin='simpleitk')
         
 def imwrite(filename, arr):
     if Path(filename).suffix in {'.tif', '.tiff'}:
@@ -15,17 +18,20 @@ def imwrite(filename, arr):
 def get_image_file_paths(input_path):
     input_path = Path(input_path)
     
+    valid_suffix = ['.tiff', '.tif', '.mhd']
     # Check if input path is a directory or a file
     if input_path.is_dir():
-        img_file_paths  = list(input_path.glob('*.tiff')) + list(input_path.glob('*.tif'))
-    elif input_path.suffix in ['.tiff', '.tif']:
+        img_file_paths = []
+        for suffix in valid_suffix:
+            img_file_paths  += list(input_path.glob('*' +suffix))
+    elif input_path.suffix in valid_suffix:
         img_file_paths  = [input_path]
     else:
-        raise ValueError('Input file format not recognized. Currently only tif files can be processed (.tif or .tiff)')
+        raise ValueError(f"Input file format not recognized. Currently only tif files can be processed ({valid_suffix})")
         
     # Check if any image files were found    
     if not img_file_paths:
-        raise ValueError("No .tiff or .tif files found in the given path.")
+        raise ValueError(f"No {valid_suffix} files found in the given path.")
         
     return img_file_paths    
 
